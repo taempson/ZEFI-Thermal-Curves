3bd6c3689576889a379e3ea398fef1077bdc4937
Michael Gilchrist <mikeg@utk.edu>
2023-02-27 21:58:04 -0500

    edits prior to working on version for  repo
    - renamed approach, for the moment, nbinom_type_I in other repo.

--- code/2023-02-26_create.overdispersed.poisson.family/qqpoisson.family.R
+++ code/2023-02-26_create.overdispersed.poisson.family/qqpoisson.family.R
@@ -2,11 +2,11 @@
 library(brms)
 
 # define custom family
-qqpoisson <- function(link_mu = "identity", link_disp = "identity")
+qqpoisson <- function(link_mu = "identity", link_theta = "identity")
     custom_family(
  name = "qqpoisson", 
  dpars = c("mu", "disp"), #expectd value and variance scale (>1)
- links = c(link_mu, link_disp), # only used if parameters are predicted
+ links = c(link_mu, link_theta), # only used if parameters are predicted
  lb = c(0, 1),
  ub = c(NA, NA),
  type = "int" #category of response variable
@@ -26,13 +26,13 @@ qqpoisson <- function(link_mu = "identity", link_disp = "identity")
 ## Using `theta` as a variable gives me problems when trying to fit model, so using `disp` instead
 ##
 stan_funs <- "
-  real qqpoisson_lpmf(int y, real mu, real disp) {
-    return neg_binomial_2_lpmf(y | mu, mu/(disp-1));
+  real qqpoisson_lpmf(int y, real mu, real theta) {
+    return neg_binomial_2_lpmf(y | mu, mu/(theta));
   }
-  int qqpoisson_rng(real mu, real disp) {
-    return neg_binomial_2_rng(mu, mu/(disp-1));
+  int qqpoisson_rng(real mu, real theta) {
+    return neg_binomial_2_rng(mu, mu/(theta));
   }
 "
-## export stan_funs to stan
+## export stan_funs to stanvars
 stanvars <- stanvar(scode = stan_funs, block = "functions")
 
