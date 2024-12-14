b911597b4c508aab43f140d0d83d287d55b7948c
taempson <111013110+taempson@users.noreply.github.com>
2024-12-14 11:41:39 -0500

    working on how to get duration for songs
    Song of the timestamps recorded for files don't make sense (some are way too long and some are negative time). I am trying to get duration of the audio files themselves, but some of these (800) don't seem to have saved the cropping that Tamara did.
    I am hopping to use some combination of either her written timestamps or the length of the files to determine song duration

---
+++ code/Empson/2024.12.14_duration of audio files-second pass.R
@@ -0,0 +1,28 @@
+
+
+install.packages("av")
+library(av)
+library(tidyverse)
+
+setwd("/Volumes/ZEFI_Videos/clipped songs")
+files <- list.files(pattern = ".wav")
+new.data<- data.frame(NA,NA)
+colnames(new.data) <- c("i.file","duration")
+for (i in 1:length(files)) {
+    tryCatch(
+        expr = {
+            i.file <- files %>% head(i) %>% tail(1)
+            i.info <- av_media_info(i.file)
+            duration<- i.info$duration
+            new.data<- new.data %>% add_row(i.file,duration)
+        },
+        error = function(cond){
+            duration<- NA
+            new.data<- new.data %>% add_row(i.file,duration)
+        }
+    )
+}
+new.data <- new.data %>% mutate(clipped_file_name = str_remove_all(i.file, ".wav")) %>% filter(!is.na(duration)) %>% select(clipped_file_name,duration)
+
+
+
